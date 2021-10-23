0553b01f6a2337c41f6293b6d62d2207a3a9bc4e
Bayu Cakra <bayu.abimanyu@stockbit.com>
2021-10-23 21:06:22 +0700

    feat(post): add feature like in post component

--- src/Post.js
+++ src/Post.js
@@ -1,14 +1,58 @@
-import React, { forwardRef } from "react";
-import "./Post.css";
+import React, { forwardRef, useContext, useEffect, useState } from "react";
 import { Avatar } from "@material-ui/core";
-import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
-import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
-import RepeatIcon from "@material-ui/icons/Repeat";
-import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-import PublishIcon from "@material-ui/icons/Publish";
+import {
+  FavoriteBorder as FavoriteBorderIcon,
+  Favorite as FavoriteIcon,
+  Repeat as RepeatIcon,
+  ChatBubbleOutline as ChatBubbleOutlineIcon,
+  VerifiedUser as VerifiedUserIcon,
+  Publish as PublishIcon
+} from "@material-ui/icons";
+import "./Post.css";
+import { AuthContext } from './FireBaseContext'
+import db from "./firebase";
 
 const Post = forwardRef(
-  ({ displayName, username, verified, text, image, avatar }, ref) => {
+  ({ id, displayName, username, verified, text, image, avatar }, ref) => {
+    const [isLike, setIsLike] = useState(false)
+    const [likeCount, setLikeCount] = useState(0)
+    const { user } = useContext(AuthContext)
+    const postLikesCollection = db.collection('post_likes')
+
+    const handleClickLike = (e) => {
+      const dataObj = {
+        post_id: id,
+        email: user.email,
+        created_at: new Date()
+      }
+
+      setIsLike(true)
+      
+      postLikesCollection.add(dataObj) 
+    }
+
+    const handleClickUnlike = (e) => {
+      setIsLike(false)
+
+      postLikesCollection
+        .where('post_id', '==', id)
+        .where('email', '==', user.email)
+        .get()
+        .then((docs) => docs.forEach((doc) => doc.ref.delete()))
+    }
+
+    useEffect(() => {
+      postLikesCollection
+        .where('post_id', '==', id)
+        .onSnapshot((snapshot) => {
+          const data = snapshot.docs.map((doc) => doc.data())
+          const isUserLiked = data.some((row) => row.post_id === id && row.email === user.email)
+
+          setIsLike(isUserLiked)
+          setLikeCount(data.length)
+        })
+    })
+
     return (
       <div className="post" ref={ref}>
         <div className="post__avatar">
@@ -33,7 +77,14 @@ const Post = forwardRef(
           <div className="post__footer">
             <ChatBubbleOutlineIcon fontSize="small" />
             <RepeatIcon fontSize="small" />
-            <FavoriteBorderIcon fontSize="small" />
+            <div>
+              {isLike ? (
+                <FavoriteIcon onClick={handleClickUnlike} fontSize="small" style={{ cursor: 'pointer', color: 'red' }} />
+              ) : (
+                <FavoriteBorderIcon onClick={handleClickLike} fontSize="small" style={{ cursor: 'pointer' }} />
+              )}
+              <small className="post__likeCount">{likeCount}</small>
+            </div>
             <PublishIcon fontSize="small" />
           </div>
         </div>
